6e1f6550d84f1f8a1119657171f22cd6d0aa51c4
dale <dale.shan@liferay.com>
2014-03-24 15:47:22 +0800

    LPS-44894 Add necessary method implements for TaskEntryAssetRender

--- portlets/tasks-portlet/docroot/WEB-INF/src/com/liferay/tasks/asset/TasksEntryAssetRenderer.java
+++ portlets/tasks-portlet/docroot/WEB-INF/src/com/liferay/tasks/asset/TasksEntryAssetRenderer.java
@@ -34,6 +34,7 @@ import com.liferay.tasks.util.WebKeys;
 import java.util.Locale;
 
 import javax.portlet.PortletRequest;
+import javax.portlet.PortletResponse;
 import javax.portlet.PortletURL;
 import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
@@ -64,6 +65,13 @@ public class TasksEntryAssetRenderer extends BaseAssetRenderer {
 
 	@Override
 	public String getSummary(Locale locale) {
+		return _entry.getTitle();
+	}
+
+	@Override
+	public String getSummary(
+		PortletRequest portletRequest, PortletResponse portletResponse) {
+
 		return _entry.getTitle();
 	}
 
