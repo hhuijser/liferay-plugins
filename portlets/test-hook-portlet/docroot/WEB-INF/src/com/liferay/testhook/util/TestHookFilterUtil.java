474f7cae824e51f31f698d3ece91a1b28a5ee928
ericmin <eric.min@liferay.com>
2014-07-30 15:50:33 +0800

    LPS-46155 Add dummy filters to test the filter order

---
+++ portlets/test-hook-portlet/docroot/WEB-INF/src/com/liferay/testhook/util/TestHookFilterUtil.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.testhook.util;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Eric Min
+ */
+public class TestHookFilterUtil {
+
+	public static class FilterNameList {
+
+		public static void addClassName(String className) {
+			if (!_classNames.contains(className)) {
+				_classNames.add(className);
+			}
+		}
+
+		public static List<String> getClassNames() {
+			return _classNames;
+		}
+
+		private static List<String> _classNames = new ArrayList<String>();
+	}
+
+}
